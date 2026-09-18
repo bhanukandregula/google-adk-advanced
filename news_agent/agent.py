44b2adc3e1865561ca5df1bbb08934ea0f29fad1
Bhanu Prakash Kandregula <bkandreg@techartsoft.com>
2026-09-18 04:41:32 -0400

    news_agent and tools from google_search

---
+++ news_agent/agent.py
@@ -0,0 +1,17 @@
+import os
+
+import google.auth
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+_, project_id = google.auth.default()
+os.environ["GOOGLE_CLOUD_PROJECT"] = project_id
+os.environ["GOOGLE_CLOUD_LOCATION"] = "global"
+os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "True"
+
+root_agent = Agent(
+    name="news_agent",
+    model="gemini-3-flash-preview",
+    instruction="You are a helpful AI assistant designed to provide accurate and useful information.",
+    tools=[google_search],
+)
